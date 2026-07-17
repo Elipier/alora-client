71f4d2b19b5df9ce4b92d2d3a8f728cfc5f72f61
Pierre Penel <47198311+pipen95@users.noreply.github.com>
2026-07-17 15:27:53 -0400

    [#20] - Défilement ok

---
+++ alora-client/src/swipeSentences.ts
@@ -0,0 +1,35 @@
+import { getFromLocalstorage, removeFromLocalstorage } from "./sentenceStorage";
+
+const sentenceDisplay = document.querySelector<HTMLElement>(".js-text-show");
+const previousButton = document.querySelector<HTMLElement>(".js-previous");
+const nextButton = document.querySelector<HTMLElement>(".js-next");
+let currentIndex = 0;
+
+const spamTracker = getFromLocalstorage("ads-candidate-feedback-hash");
+if (spamTracker) {
+  removeFromLocalstorage("ads-candidate-feedback-hash");
+}
+
+const localStorageArray = Object.values(localStorage);
+const arrayLength = localStorageArray.length;
+
+console.log(localStorageArray);
+
+if (sentenceDisplay && previousButton && nextButton) {
+  sentenceDisplay.textContent = `${localStorageArray[currentIndex]}`;
+  previousButton.addEventListener("click", () => {
+    const prevIndex = (currentIndex + arrayLength - 1) % arrayLength;
+    const prevItem = localStorageArray[prevIndex];
+    sentenceDisplay.textContent = `${prevItem}`;
+    currentIndex = prevIndex;
+    console.log(currentIndex);
+  });
+
+  nextButton.addEventListener("click", () => {
+    const nextIndex = (currentIndex + 1) % arrayLength;
+    const nextItem = localStorageArray[nextIndex];
+    sentenceDisplay.textContent = `${nextItem}`;
+    currentIndex = nextIndex;
+    console.log(currentIndex);
+  });
+}
